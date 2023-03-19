53ce6c4279d62a879e2fccbfb51be4bfddedcb87
zhanghaoyang0 <zhanghaoyang0@hotmail.com>
2023-03-19 18:35:30 +0800

    update dbnsfp

--- code/.notrun.py
+++ code/.notrun.py
@@ -2,8 +2,8 @@
 ### this script is only for me to check something
 
 ### add soft link
-# ln -s ../db/annovar ./annovar # link to in-house db
-# rm -rf annovar
+ln -s ../../db/annovar ./annovar # link to in-house db
+rm -rf annovar
 
 ### note
 # if input have NaN in the int column, the output of the int col will be float col. 
